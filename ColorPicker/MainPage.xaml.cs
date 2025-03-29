17661a8d90c3aaf9cfa090d63348309453d1ddce
pengibot <pengibot@gmail.com>
2025-03-29 07:57:03 +0000

    Uploaded code

---
+++ ColorPicker/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿namespace ColorPicker;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        this.Loaded += MainPage_Loaded;
+    }
+
+    private void MainPage_Loaded(object? sender, EventArgs e)
+    {
+        Color color = Color.FromRgb(0, 0, 0);
+        SetBackgroundColor(color);
+    }
+
+    private void slrValue_ValueChanged(object? sender, ValueChangedEventArgs e)
+    {
+        var red = rValue.Value;
+        var green = gValue.Value;
+        var blue = bValue.Value;
+
+        Color color = Color.FromRgb(red, green, blue);
+
+        SetBackgroundColor(color);
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var random = new Random();
+
+        var red = random.Next(0, 255);
+        var green = random.Next(0, 255);
+        var blue = random.Next(0, 255);
+
+        rValue.Value = red / 255.0;
+        gValue.Value = green / 255.0;
+        bValue.Value = blue / 255.0;
+
+        var color = Color.FromRgb(red, green, blue);
+
+        SetBackgroundColor(color);
+    }
+
+    private void SetBackgroundColor(Color color)
+    {
+        lblInfo.Text = "";
+        AppBackground.BackgroundColor = color;
+        rgbValue.Text = color.ToHex();
+    }
+
+    private async void copyBtn_Clicked(object sender, EventArgs e)
+    {
+        await Clipboard.SetTextAsync(rgbValue.Text);
+        lblInfo.Text = "Copied to clipboard";
+    }
+}
